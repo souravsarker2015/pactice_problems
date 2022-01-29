12d81c43797cd2dbb30bb19326ef53f8ee56d93a
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-01-29 16:51:34 +0600

    selection sort

---
+++ data structure and algorithm/sorting/3.2 selection_sort.py
@@ -0,0 +1,21 @@
+class SelectionSort:
+    def __init__(self, data):
+        self.data = data
+
+    def selection_sort(self):
+        length = len(self.data)
+        for i in range(length - 1):
+            min = i
+            for j in range(min + 1, length):
+                if self.data[j] < self.data[min]:
+                    min = j
+
+            if i != min:
+                self.data[min], self.data[i] = self.data[i], self.data[min]
+
+
+if __name__ == "__main__":
+    ele = [1, 6, 2, 4, 3, 9, 8]
+    s = SelectionSort(ele)
+    s.selection_sort()
+    print(ele)
